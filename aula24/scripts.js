5981b0d1470abcdde65bf95eeba615635ea4db83
adsonrocha <adsonrocha@gmail.com>
2022-10-03 16:56:47 -0300

    .

---
+++ aula24/scripts.js
@@ -0,0 +1,14 @@
+function trocar() {
+    document.getElementById('img1').src = "../img/cachorro2.webp"
+}
+
+function redimensionar() {
+    document.getElementById('img1').style.width = "640px"
+    document.getElementById('img1').style.height = "480px"
+}
+
+function redefinir() {
+    document.getElementById('img1').style.width = "320px"
+    document.getElementById('img1').style.height = "240px"
+    document.getElementById('img1').src = "../img/cachorro.jpg"
+}
